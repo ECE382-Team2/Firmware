01b5aa6e28eec0cedbce5ba80a801e4375542e51
Will-est <rwillpainter@gmail.com>
2025-10-13 19:46:44 -0500

    made code between board and chip the same

---
+++ PSOC_Workspace/PSOC_Project.cydsn/cyapicallbacks.c
@@ -0,0 +1,165 @@
+/*******************************************************************************
+* File Name: cyapicallbacks.c
+*
+* Version: 1.0
+* 
+* Description: This file contains the function that is used to change the 
+*              inactive sensor connections based on the sensor that is
+*              being scanned currently.
+*
+* Hardware Dependency: This example supports the following kits:
+*                      CY8CKIT-149 PSoC® 4100S Plus Prototyping Kit,   
+*                      CY8CKIT-145-40XX PSoC® 4000S CapSense Prototyping Kit.
+******************************************************************************
+* Copyright (2020), Cypress Semiconductor Corporation.
+******************************************************************************
+* This software, including source code, documentation and related materials
+* ("Software") is owned by Cypress Semiconductor Corporation (Cypress) and is
+* protected by and subject to worldwide patent protection (United States and 
+* foreign), United States copyright laws and international treaty provisions. 
+* Cypress hereby grants to licensee a personal, non-exclusive, non-transferable
+* license to copy, use, modify, create derivative works of, and compile the 
+* Cypress source code and derivative works for the sole purpose of creating 
+* custom software in support of licensee product, such licensee product to be
+* used only in conjunction with Cypress's integrated circuit as specified in the
+* applicable agreement. Any reproduction, modification, translation, compilation,
+* or representation of this Software except as specified above is prohibited 
+* without the express written permission of Cypress.
+* 
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, 
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+* Cypress reserves the right to make changes to the Software without notice. 
+* Cypress does not assume any liability arising out of the application or use
+* of Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use as critical components in any products 
+* where a malfunction or failure may reasonably be expected to result in 
+* significant injury or death ("ACTIVE Risk Product"). By including Cypress's 
+* product in a ACTIVE Risk Product, the manufacturer of such system or application
+* assumes all risk of such use and in doing so indemnifies Cypress against all
+* liability. Use of this Software may be limited by and subject to the applicable
+* Cypress software license agreement.
+*****************************************************************************/
+
+#include "cyapicallbacks.h"
+
+/*******************************************************************************
+* Function Name: CapSense_StartSampleCallback
+********************************************************************************
+* Summary:
+* This function is called before the CapSense scan begins.
+* The drive mode of the sensors which are adjacent to the sensor being scanned 
+* is set to shield and all other inactive sensors are connected to ground.
+*
+* Parameters:
+*  \param currentWidgetIndex
+*  Specifies the ID number of the widget that is going to be
+*  scanned.
+*  A macro for the widget ID can be found in the CapSense Configuration header
+*  file defined as CapSense_<WidgetName>_WDGT_ID.
+*
+* \param currentSensorIndex
+*  Specifies the ID number of the sensor within the widget that is
+*  going to be scanned.
+*  A macro for the sensor ID within a specified widget can be found in the
+*  CapSense Configuration header file defined as
+*  CapSense_<WidgetName>_SNS<SensorNumber>_ID
+*
+*******************************************************************************/
+
+
+#include <stdint.h> // Required for uint_fast8_t and uint8_t
+#include "globals.h"
+
+// Helper functions
+// this was made before I realized that I could just not read the invalid sensor values(which I already do)
+// so it is here in case it will be useful, but otherwise it just won't be used
+uint8_t is_valid_index(uint8_t index)
+{
+    // Check if we are in the special mode
+    if (mode_flag == 0)
+    {
+        // Use a switch statement to check against the list of valid indexes
+        switch (index)
+        {
+            case 0:
+            case 2:
+            case 3:
+            case 4:
+                return 1; // It's a valid index for mode 0
+
+            default:
+                return 0; // It's an invalid index for mode 0
+        }
+    }
+    
+    // For any mode other than 0, all indexes are considered valid.
+    return 1;
+}
+
+
+
+
+// This function assumes 'mode_flag' is a global variable accessible here.
+// For example: extern volatile uint8_t mode_flag;
+
+/**
+ * @brief Checks if a given index is valid based on the current mode.
+ * @param index The index to check.
+ * @return 1 if the index is valid, 0 otherwise.
+ */
+
+void CapSense_StartSampleCallback (uint32 currentWidgetIndex, uint32 currentSensorIndex)
+{
+   
+    uint8 sensorIndex;
+    
+//    if(currentWidgetIndex == CapSense_LINEARSLIDER_WDGT_ID)
+//    {
+//        for(sensorIndex = 0; sensorIndex < CapSense_LINEARSLIDER_NUM_SENSORS; sensorIndex++)
+//        {
+//            if(sensorIndex != currentSensorIndex)
+//            {
+//                if((sensorIndex == (currentSensorIndex - 1)) || (sensorIndex == (currentSensorIndex + 1)))
+//                {
+//                    // If the sensor is adjacent to the sensor being scanned, 
+//                     // configure it as shield. 
+//                     
+//                    CapSense_SetPinState(CapSense_LINEARSLIDER_WDGT_ID, sensorIndex, CapSense_SHIELD);
+//                }
+//                else
+//                {
+//                    // If the sensor is not adjacent to the sensor being 
+//                    // scanned, connect it to ground
+//                    CapSense_SetPinState(CapSense_LINEARSLIDER_WDGT_ID, sensorIndex, CapSense_GROUND);
+//                }
+//            }
+//        }
+//    }
+    
+    if(currentWidgetIndex == CapSense_PROXIMITY0_WDGT_ID)
+    {
+        for(sensorIndex = 0; sensorIndex < 1; sensorIndex++)
+        {
+            if((sensorIndex != currentSensorIndex))
+            {
+                if((sensorIndex == (currentSensorIndex - 1)) || (sensorIndex == (currentSensorIndex + 1)))
+                {
+                    // If the sensor is adjacent to the sensor being scanned, 
+                     // configure it as shield. 
+                     
+                    CapSense_SetPinState(CapSense_PROXIMITY0_WDGT_ID, sensorIndex, CapSense_SHIELD);
+                }
+                else
+                {
+                    // If the sensor is not adjacent to the sensor being 
+                    // scanned, connect it to ground
+                    CapSense_SetPinState(CapSense_PROXIMITY0_WDGT_ID, sensorIndex, CapSense_GROUND);
+                }
+            }
+        }
+    }
+}
+
+
+/* [] END OF FILE */
--- PSOC_Workspace/PSOC_Project.cydsn/main.c
+++ PSOC_Workspace/PSOC_Project.cydsn/main.c
@@ -1,26 +1,271 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
+/*****************************************************************************
+* File Name: main.c
+*****************************************************************************/
+/*******************************************************************************
+* Included Headers
+*******************************************************************************/
 #include "project.h"
+#include <stdio.h>
+#include <string.h>
 
-int main(void)
+/*******************************************************************************
+* MACRO Definitions
+*******************************************************************************/
+/* Boolean constants for LED control */
+#define LED_ON           (0u)
+#define LED_OFF          (1u)
+
+/* Define the size for the serial message buffer */
+#define TX_MESSAGE_SIZE  64
+
+/* Calibration Constants */
+#define CALIB_NUM_SAMPLES 50
+#define CALIB_DELAY_MS
+
+/*****************************************************************************
+* Function Prototypes & Global Variables
+*****************************************************************************/
+void UART_PutString(const char *s);
+void Post_Process(void);
+void CalibrateCapSense(uint32 widgetID);
+void DetectTouchAndDriveLed(void);
+
+
+uint16 raw_count; 
+uint8_t mode_flag = 0; // positive = shear, zero = normal
+int16_t processed_data_array[4];
+
+// Functions
+
+/*******************************************************************************
+* Function Name: Post_Process()
+********************************************************************************
+* Summary:
+* Changes the processed_data_array depending on the mode
+*
+* Parameters:
+*
+* Return:
+* None
+*******************************************************************************/
+
+/**
+ * @brief Processes CapSense raw counts using the recommended CapSense API.
+ *
+ * This function assumes the following are available:
+ * - A CapSense widget named "Proximity0" (adjust macro if name is different).
+ * - volatile uint8_t mode_flag;
+ * - int16_t processed_array[4];
+ */
+void Post_Process(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    uint8_t i;
+    uint32_t widgetId = 0u; // Assuming proximity widget is index 0 — change if needed
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
-    for(;;)
+    // --- Mode 0: Read individual, evenly-spaced sensors ---
+    if (mode_flag == 0)
     {
-        /* Place your application code here. */
+        // The specific proximity sensor IDs you want to read
+        const uint8_t sensor_element_indexes[4] = {0, 2, 4, 6};
+
+        for (i = 0; i < 4; i++)
+        {
+            // Read the raw count for each specified proximity sensor
+            // I personally feel like I should sum subsequent indexes but idk, will test maybe on real board.
+            processed_data_array[i] = CapSense_dsRam.snsList.proximity0[sensor_element_indexes[i]].raw[0];
+        }
+    }
+    // --- Mode 1: Read sensor pairs and find the difference ---
+    else
+    {
+        for (i = 0; i < 4; i++)
+        {
+            uint8_t base_element_index = i * 2;
+
+            // Read both raw counts from the pair
+            uint16_t sensor_a = CapSense_dsRam.snsList.proximity0[base_element_index].raw[0];
+            uint16_t sensor_b = CapSense_dsRam.snsList.proximity0[base_element_index+1].raw[0];
+
+            // Store the difference
+            processed_data_array[i] = sensor_a - sensor_b;
+        }
     }
 }
+
+
+
+/*******************************************************************************
+* Function Name: UART_PutString_Robust()
+********************************************************************************
+* Summary:
+* Sends a null-terminated string character-by-character using the lowest-level
+* SCB UART transmit API (UART_SpiUartWriteTxData).
+*
+* Parameters:
+* s: Pointer to the null-terminated string to transmit.
+*
+* Return:
+* None
+*******************************************************************************/
+void UART_PutString(const char *s)
+{
+    while (*s != '\0') {
+        UART_SpiUartWriteTxData((uint32)*s);
+        s++;
+    }
+}
+
+// /*******************************************************************************
+// * Function Name: IsShieldingActive
+// ********************************************************************************
+// * Summary:
+// * Checks the CapSense configuration data structure for the Active Shield flag.
+// * According to the CapSense register map, 
+// *   CDS0_CONFIG is a uint16
+// *   SHLD_EN, the shield electrode signal enable/disable, is bit 8
+// * Return:
+// * 1 if shielding is configured as Active, 0 otherwise.
+// *******************************************************************************/
+// uint8 IsShieldingActive(void)
+// {    
+//     if ((CapSense_dsRam.csd0Config >> 8) & 1) {
+//         return 1;  
+//     } else {
+//         return 0;  
+//     } 
+// }
+
+/*******************************************************************************
+* Function Name: CalibrateCapSense
+********************************************************************************
+* Summary:
+*
+* Parameters:
+* widgetId: The ID of the widget to calibrate (e.g., CapSense_PROXIMITY0_WDGT_ID).
+*
+* Return:
+* None
+*******************************************************************************/
+void CalibrateCapSense(uint32 widgetID) {
+    
+    char txMessage[TX_MESSAGE_SIZE];
+    uint32 status;
+        
+    // Start the tuning process. This is required before calling auto-calibration.
+    CapSense_RunTuner();
+    
+    UART_PutString("--- Starting CapSense Widget Calibration ---\r\n");
+    
+    /* CapSense block as a CalibrateWidget(). It calibrates all the IDACs for a given 
+    widget. IDAC is Current Digital-to-Analog Converter. I think it adjusts the current range?
+    
+    Returns 0 if calibration was successful. 
+    */ 
+    status = CapSense_CalibrateWidget(widgetID); 
+    sprintf(txMessage, "%lu\r\n", status); 
+    UART_PutString(txMessage);
+    
+    UART_PutString("--- Calibration Complete ---\r\n");
+    
+}
+
+
+/*******************************************************************************
+* Function Name: DetectTouchAndDriveLed
+********************************************************************************
+* Summary:
+* Reads the CapSense Proximity sensor data and transmits ONLY the raw count 
+* as a number followed by a newline via UART.
+*
+* Parameters:
+* None
+*
+* Return:
+* None
+*******************************************************************************/
+void DetectTouchAndDriveLed(void)
+{
+    // Reduce buffer size to just the raw number and necessary characters
+    char txMessage[TX_MESSAGE_SIZE]; 
+
+    // LED Control: (No Change)
+    if (CapSense_IsWidgetActive(CapSense_PROXIMITY0_WDGT_ID))
+    {
+        LED_0_Write(LED_ON);
+    }
+    else
+    {
+        LED_0_Write(LED_OFF);
+    }
+    
+    // *** CRITICAL CHANGE: Only format the number and newline characters ***
+    // This is the most streamlined way to log data.
+    
+    // prints each value in the processed array with the corresponding electrode
+        // Format the string with the mode, electrode index, and processed count
+    
+        uint mode_bit = (mode_flag == 0) ? 0 : 1;
+        sprintf(txMessage, "\n%u,%d,%d,%d,%d\r", 
+                mode_bit,  
+                processed_data_array[0],
+                processed_data_array[1],
+                processed_data_array[2],
+                processed_data_array[3]);
+        
+        // Send the fully formatted string over the UART
+        UART_PutString(txMessage);
+    CyDelay(1000);
+}
+
+/*******************************************************************************
+* Function Name: main()
+********************************************************************************
+* Summary:
+* Initializes components, enables interrupts, and runs the main CapSense
+* scanning loop.
+*******************************************************************************/
+int main(void)
+{
+    /* Enable global interrupts (required for CapSense/UART operation) */
+    CyGlobalIntEnable;
+
+    /* Start the UART component */
+    UART_Start();
+
+    /* Send a start message to confirm the link */
+    UART_PutString("--- PSoC CapSense Logger Initialized ---\r\n");
+
+    /* Start the CapSense block */
+    CapSense_Start();
+    
+    /* Calibrate CapSense block */
+    //CalibrateCapSense(CapSense_PROXIMITY0_WDGT_ID);
+    
+    CapSense_CalibrateAllWidgets();
+    /* Initiate the first scan of all enabled widgets */
+    CapSense_ScanAllWidgets();
+
+    for (;;)
+    {
+        /* Check if the CapSense hardware block is done with the current scan */
+        if (CapSense_NOT_BUSY == CapSense_IsBusy())
+        {
+            /* Process the raw sensor data (filtering, baseline, detection) */
+            CapSense_ProcessAllWidgets();
+            
+            // Post process the sensor data
+            Post_Process();
+            
+            /* Handle LED control and send the debug message over UART */
+            DetectTouchAndDriveLed();
+
+            // toggles the mode we are in after succesfully writing
+            mode_flag = ~(mode_flag);
+            /* Start the next scan of all enabled widgets */
+            CapSense_ScanAllWidgets();
+        }
+    }
+}
+
 
 /* [] END OF FILE */
